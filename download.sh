cf58464e4b6ecf4e952272e3df4fae39e6a3c30e
Rohan <manzoor.rohan97@gmail.com>
2023-12-18 21:40:21 +0500

    Added script to download TartanAir dataset

---
+++ download.sh
@@ -0,0 +1,15 @@
+mkdir "abandonedfactory_easy" "abandonedfactory_night_easy" "hospital_easy" "oldtown_hard"
+
+wget -P "abandonedfactory_easy/" http://airlab-share.andrew.cmu.edu/tartanair/abandonedfactory/Easy/image_left.zip
+# wget -P "abandonedfactory_easy/" http://airlab-share.andrew.cmu.edu/tartanair/abandonedfactory/Easy/image_right.zip
+
+wget -P "abandonedfactory_night_easy" http://airlab-share.andrew.cmu.edu/tartanair/abandonedfactory_night/Easy/image_left.zip
+#wget -P "abandonedfactory_night_easy" http://airlab-share.andrew.cmu.edu/tartanair/abandonedfactory_night/Easy/image_right.zip
+
+wegt -P "hospital_easy" http://airlab-share.andrew.cmu.edu/tartanair/hospital/Easy/image_left.zip
+# wegt -P "hospital_easy" http://airlab-share.andrew.cmu.edu/tartanair/hospital/Easy/image_right.zip
+
+wget -P "oldtown_hard" http://airlab-share.andrew.cmu.edu/tartanair/oldtown/Hard/depth_left.zip
+# wget -P "oldtown_hard" http://airlab-share.andrew.cmu.edu/tartanair/oldtown/Hard/depth_easy.zip
+
+
