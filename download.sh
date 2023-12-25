b7e1f32108c35f784ea2dfedd6d69ab853258323
teenaxta <manzoor.rohan97@gmail.com>
2023-12-25 11:35:13 +0500

    Added file to download dataset

--- download.sh
+++ download.sh
@@ -1,15 +1,15 @@
 mkdir "dataset/abandonedfactory_easy" "dataset/abandonedfactory_night_easy" "dataset/hospital_easy" "dataset/oldtown_hard"
 
-wget -P "abandonedfactory_easy/" http://airlab-share.andrew.cmu.edu/tartanair/abandonedfactory/Easy/image_left.zip
+wget -P "dataset/abandonedfactory_easy/" http://airlab-share.andrew.cmu.edu/tartanair/abandonedfactory/Easy/image_left.zip
 # wget -P "abandonedfactory_easy/" http://airlab-share.andrew.cmu.edu/tartanair/abandonedfactory/Easy/image_right.zip
 
-wget -P "abandonedfactory_night_easy" http://airlab-share.andrew.cmu.edu/tartanair/abandonedfactory_night/Easy/image_left.zip
+wget -P "dataset/abandonedfactory_night_easy" http://airlab-share.andrew.cmu.edu/tartanair/abandonedfactory_night/Easy/image_left.zip
 #wget -P "abandonedfactory_night_easy" http://airlab-share.andrew.cmu.edu/tartanair/abandonedfactory_night/Easy/image_right.zip
 
-wegt -P "hospital_easy" http://airlab-share.andrew.cmu.edu/tartanair/hospital/Easy/image_left.zip
+wegt -P "dataset/hospital_easy" http://airlab-share.andrew.cmu.edu/tartanair/hospital/Easy/image_left.zip
 # wegt -P "hospital_easy" http://airlab-share.andrew.cmu.edu/tartanair/hospital/Easy/image_right.zip
 
-wget -P "oldtown_hard" http://airlab-share.andrew.cmu.edu/tartanair/oldtown/Hard/depth_left.zip
+wget -P "dataset/oldtown_hard" http://airlab-share.andrew.cmu.edu/tartanair/oldtown/Hard/depth_left.zip
 # wget -P "oldtown_hard" http://airlab-share.andrew.cmu.edu/tartanair/oldtown/Hard/depth_easy.zip
 
 
