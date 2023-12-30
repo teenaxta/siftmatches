acb2865a733c45cffffbdccffd0cc368bcd80b88
teenaxta <manzoor.rohan97@gmail.com>
2023-12-30 13:26:55 +0500

    Updated download.sh script

--- download.sh
+++ download.sh
@@ -6,10 +6,10 @@ wget -P "dataset/abandonedfactory_easy/" http://airlab-share.andrew.cmu.edu/tart
 wget -P "dataset/abandonedfactory_night_easy" http://airlab-share.andrew.cmu.edu/tartanair/abandonedfactory_night/Easy/image_left.zip
 #wget -P "abandonedfactory_night_easy" http://airlab-share.andrew.cmu.edu/tartanair/abandonedfactory_night/Easy/image_right.zip
 
-wegt -P "dataset/hospital_easy" http://airlab-share.andrew.cmu.edu/tartanair/hospital/Easy/image_left.zip
+wget -P "dataset/hospital_easy" http://airlab-share.andrew.cmu.edu/tartanair/hospital/Easy/image_left.zip
 # wegt -P "hospital_easy" http://airlab-share.andrew.cmu.edu/tartanair/hospital/Easy/image_right.zip
 
-wget -P "dataset/oldtown_hard" http://airlab-share.andrew.cmu.edu/tartanair/oldtown/Hard/depth_left.zip
+wget -P "dataset/oldtown_hard" http://airlab-share.andrew.cmu.edu/tartanair/oldtown/Hard/image_left.zip
 # wget -P "oldtown_hard" http://airlab-share.andrew.cmu.edu/tartanair/oldtown/Hard/depth_easy.zip
 
 
